16c48391bacbc38679b5d9b270de0624e818fd39
apsdeveloper461 <mehboobalam786461@gmail.com>
2025-05-18 02:17:54 +0500

    add search bar in home page

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,30 +1,54 @@
+import { CiSearch } from "react-icons/ci";
 import { Link } from "react-router-dom";
 
 const Main = ({
-                heading1,
-                heading2,
-                heading3,
-                subtitle,
-                btn1text,
-                btn2text,
-                home = false
-              }) => {
+  heading1,
+  heading2,
+  heading3,
+  subtitle,
+  btn1text,
+  btn2text,
+  isSearch=false,
+  setSearchData,
+  searchData,
+  home = false,
+}) => {
   return (
     <main className={`main pad ${home && "main__home"}`}>
-      <div className="main__content">
-        <h1 className="heading moveInLeft">
-          <span className="heading heading--primary">{heading1 + " "}</span>
-          {heading2 &&
-            <span className="heading heading--secondary">{heading2+" "}</span>
-          }
-          {heading3 &&
-            <span className="heading heading--primary">{heading3}</span>
-          }
+      <div className='main__content'>
+        <h1 className='heading '>
+          <span className='heading heading--primary'>{heading1 + " "}</span>
+          {heading2 && (
+            <span className='heading heading--secondary'>{heading2 + " "}</span>
+          )}
+          {heading3 && (
+            <span className='heading heading--primary'>{heading3}</span>
+          )}
         </h1>
-        {subtitle && <p className="heading--subtitle">
-          Tips, Guidelines and Resources For Smarter Parking and Safer Driving.
-        </p>
-        }
+        {/* Search Bar Start */}
+    {isSearch && (
+          <div className='main__searchbar'>
+          <input
+            type='text'
+            value={searchData}
+            onChange={(e) => setSearchData(e.target.value)}
+            className='main__searchinput'
+            placeholder='Search tips, guides, or topics...'
+            aria-label='Search'
+          />
+          <span className='main__searchicon'>
+            {/* Using react-icons for search icon */}
+            <CiSearch style={{fontSize:"20px"}} />
+          </span>
+        </div>
+        )}
+        {/* Search Bar End */}
+        {subtitle && (
+          <p className='heading--subtitle'>
+            Tips, Guidelines and Resources For Smarter Parking and Safer
+            Driving.
+          </p>
+        )}
         {(btn1text || btn2text) && <div className="main--btn moveInUp">
           {btn1text && <Link to="/" className="btn btn-primary">
             Get A PCN <span className="button__icon">↗</span>
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -4,16 +4,23 @@ import Cards from "../components/Card.jsx";
 import Footer from "../components/Footer.jsx";
 import Main from "../components/Main.jsx";
 import InfoBanner from "../components/InfoBanner.jsx";
+import { useEffect, useState } from "react";
 
 const HomePage = () => {
+  const [searchData, setSearchData] = useState("");
+  // useEffect(()=>{
+  //   console.log("searchData", searchData);
+  // },[searchData])
   return (<div>
     <NavBar />
     <Main heading1="Driver" heading2="Knowledge" heading3="Hub"
           subtitle="Tips, Guidelines and Resources For Smarter Parking and Safer Driving"
-          btn1text="Get A PCN" btn2text="Get A Free Ticket" home={true} />
+          searchData={searchData}
+          setSearchData={setSearchData}
+          isSearch={true} home={true} />
 
     <InfoBanner />
-    <Cards />
+    <Cards search={searchData} />
     <Footer />
   </div>);
 };
