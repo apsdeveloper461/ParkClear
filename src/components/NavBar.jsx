75a0c5be0ea70a58be8aad5b5856b0227be67b0e
apsdeveloper461 <mehboobalam786461@gmail.com>
2025-05-18 19:14:31 +0500

    logo cahnge for small device

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -9,7 +9,7 @@ const NavBar = () => {
     const menuButton = document.querySelector(".sm_menu-button");
     const menu = document.querySelector(".sm_navbar__menu");
 
-    document?.addEventListener("click", function(event) {
+    document?.addEventListener("click", function (event) {
       if (!menu?.contains(event.target) && event.target !== menuButton) {
         menu?.classList.remove("sm_navbar__menu-active");
       }
@@ -28,70 +28,133 @@ const NavBar = () => {
 
   // this navbar is for smaller screens
   const navBarSmall = () => {
-    return (<Fragment>
-      <nav className={`navbar pad ${isSticky ? "sticky-nav" : ""}`}>
-        <div className="navbar__logo-container">
-          <img src="/logo1.png" className="navbar__logo" alt="" />
-        </div>
-        <button className="sm_menu-button" onClick={e => {
-          e.preventDefault();
-          document.querySelector(".sm_navbar__menu").
-            classList.
-            toggle("sm_navbar__menu-active");
-        }}>
-          <i className="fas fa-bars" style={{ pointerEvents: "none" }}></i>
-        </button>
-      </nav>
+    // Choose logo based on window width
+    const logoSrc = window.innerWidth > 900 ? "/logo1.png" : "/logoMobile.png";
+    return (
+      <Fragment>
+        <nav className={`navbar pad ${isSticky ? "sticky-nav" : ""}`}>
+          <div className='navbar__logo-container'>
+            <img
+              src={logoSrc}
+              className='navbar__logo'
+              alt=''
+            />
+          </div>
+          <button
+            className='sm_menu-button'
+            onClick={(e) => {
+              e.preventDefault();
+              document
+                .querySelector(".sm_navbar__menu")
+                .classList.toggle("sm_navbar__menu-active");
+            }}
+          >
+            <i
+              className='fas fa-bars'
+              style={{ pointerEvents: "none" }}
+            ></i>
+          </button>
+        </nav>
 
-      <div className="sm_navbar__menu">
-        <Link to="/" className="navbar__link sm_navbar__item">Home</Link>
-        <Link to="/" className="navbar__link sm_navbar__item">Manage
-          Tickets</Link>
-        <Link to="/"
-              className="navbar__link navbar__link--active sm_navbar__item">
-          Driver Knowledge Hub
-        </Link>
-        <Link to="/" className="navbar__link sm_navbar__item">Restore
-          Images</Link>
-        <Link to="/" className="navbar__link sm_navbar__item">About Us</Link>
-        <Link to="/" className="sm_navbar__item">
-          <button className="btn btn-primary">Log In</button>
-        </Link>
-        <Link to="/" className="btn btn-secondarysm_navbar__item">
-          <button className="btn btn-secondary">Register</button>
-        </Link>
-      </div>
-    </Fragment>);
+        <div className='sm_navbar__menu'>
+          <Link
+            to='/'
+            className='navbar__link sm_navbar__item'
+          >
+            Home
+          </Link>
+          <Link
+            to='/'
+            className='navbar__link sm_navbar__item'
+          >
+            Manage Tickets
+          </Link>
+          <Link
+            to='/'
+            className='navbar__link navbar__link--active sm_navbar__item'
+          >
+            Driver Knowledge Hub
+          </Link>
+          <Link
+            to='/'
+            className='navbar__link sm_navbar__item'
+          >
+            Restore Images
+          </Link>
+          <Link
+            to='/'
+            className='navbar__link sm_navbar__item'
+          >
+            About Us
+          </Link>
+          <Link
+            to='/'
+            className='sm_navbar__item'
+          >
+            <button className='btn btn-primary'>Log In</button>
+          </Link>
+          <Link
+            to='/'
+            className='btn btn-secondarysm_navbar__item'
+          >
+            <button className='btn btn-secondary'>Register</button>
+          </Link>
+        </div>
+      </Fragment>
+    );
   };
 
   // this navbar is for bigger screens
   const navBarBig = () => {
-    return (<nav className={`navbar pad ${isSticky ? "sticky-nav" : ""}`}>
-      <div className="navbar__logo-container">
-        <img src="/logo1.png" alt="Logo"
-             className="navbar__logo" />
-      </div>
-      <ul className="navbar__menu">
-        <li>
-          <Link to="/" className="navbar__link">Home</Link>
-        </li>
-        <li>
-          <Link to="/" className="navbar__link">Manage Tickets</Link>
-        </li>
-        <li>
-          <Link to="/" className="navbar__link navbar__link--active">
-            Driver Knowledge Hub
-          </Link>
-        </li>
-        <li>
-          <Link to="/" className="navbar__link">About Us</Link>
-        </li>
-      </ul>
-      <div className="navbar__auth">
-        <button className="btn btn-primary">Log In</button>
-        <button className="btn btn-secondary">Register</button>
-      </div>
-    </nav>);
+    return (
+      <nav className={`navbar pad ${isSticky ? "sticky-nav" : ""}`}>
+        <div className='navbar__logo-container'>
+          <img
+            src='/logo1.png'
+            alt='Logo'
+            className='navbar__logo'
+          />
+        </div>
+        <ul className='navbar__menu'>
+          <li>
+            <Link
+              to='/'
+              className='navbar__link'
+            >
+              Home
+            </Link>
+          </li>
+          <li>
+            <Link
+              to='/'
+              className='navbar__link'
+            >
+              Manage Tickets
+            </Link>
+          </li>
+          <li>
+            <Link
+              to='/'
+              className='navbar__link navbar__link--active'
+            >
+              Driver Knowledge Hub
+            </Link>
+          </li>
+          <li>
+            <Link
+              to='/'
+              className='navbar__link'
+            >
+              About Us
+            </Link>
+          </li>
+        </ul>
+        <div className='navbar__auth'>
+          <button className='btn btn-primary'>Log In</button>
+          <button className='btn btn-secondary'>Register</button>
+        </div>
+      </nav>
+    );
   };
 
   return window.innerWidth > 900 ? navBarBig() : navBarSmall();
